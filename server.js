cf25008d8607ee1b6d35058aa37359971321b84b
haloandfable <tylerallen@live.com>
2022-12-10 13:13:44 -0500

    m

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
-  static: './build'
+  static: './build',
 });
 const PORT = process.env.PORT || 9000;
 server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}))
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
 server.use(router);
 server.listen(PORT, () => {
   console.log('Server is running');
 });
 
+var http = require('http');
+setInterval(function () {
+  http.get('https://contactmanager.tyler-allen.com');
+}, 300000); // every 5 minutes (300000)
 
-// server
+
