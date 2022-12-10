9ae5038a569a1c8ebb8779f2a87243b91015929b
haloandfable <tylerallen@live.com>
2022-12-10 13:59:23 -0500

    m

--- server.js
+++ server.js
@@ -16,9 +16,5 @@ server.listen(PORT, () => {
   console.log('Server is running');
 });
 
-var http = require('http');
-setInterval(function () {
-  http.get('https://contactmanager.tyler-allen.com');
-}, 300000); // every 5 minutes (300000)
 
 
