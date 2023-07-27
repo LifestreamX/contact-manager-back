1ff82c6a934af87250629fd7bbec935ace0f6a88
haloandfable <tylerallen@live.com>
2023-07-27 14:27:44 -0400

    u

--- api/server.js
+++ api/server.js
@@ -1,11 +1,10 @@
 const jsonServer = require('json-server');
+const { fs } = require('memfs'); // Import memfs
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
   static: './build',
 });
-const { fs } = require('memfs'); // Import memfs
-
 
 // Import and use micro-cors middleware
 const cors = require('micro-cors')();
