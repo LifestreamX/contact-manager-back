895efde5a641279c118ee25b2db8e5f6eb07e33e
haloandfable <tylerallen@live.com>
2023-07-27 14:30:43 -0400

    u

--- api/server.js
+++ api/server.js
@@ -4,35 +4,17 @@ const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
   static: './build',
 });
-
 const PORT = process.env.PORT || 9000;
-
-// Enable CORS for all origins
-server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 server.use(middlewares);
-
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
   })
 );
-
-// Intercept the router.db object with the in-memory file system
-router.db._.write = () => {
-  fs.writeFileSync('./db.json', JSON.stringify(router.db.getState()));
-};
-
-// Load initial data from the file system
-router.db.setState(JSON.parse(fs.readFileSync('./db.json', 'utf8')));
-
 server.use(router);
-
 server.listen(PORT, () => {
   console.log('Server is running');
 });
+
+
+
