b5173a9fde404822fe17aecf93d5e12c142156fc
haloandfable <tylerallen@live.com>
2023-07-27 13:04:36 -0400

    server update

--- api/server.js
+++ api/server.js
@@ -1,3 +1,5 @@
+const { createServer } = require('http');
+const { parse } = require('url');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
@@ -5,16 +7,26 @@ const middlewares = jsonServer.defaults({
   static: './build',
 });
 const PORT = process.env.PORT || 9000;
+
+// Disable caching for API responses
+server.use((req, res, next) => {
+  res.header('Cache-Control', 'no-store');
+  next();
+});
+
 server.use(middlewares);
+
+// Rewrite rules to handle API routes
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
   })
 );
+
 server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running');
+
+const httpServer = createServer(server);
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
-
